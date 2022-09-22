06afcc759a552a175805495125b570704756de75
adarshzpatel@gmail.com <adarshzpatel@gmail.com>
2022-09-22 15:26:10 +0530

    refactor : roadmap

--- src/components/homepage/Roadmap.tsx
+++ src/components/homepage/Roadmap.tsx
@@ -27,14 +27,6 @@ const Roadmap = (props: Props) => {
           <div className="absolute -left-[11px] h-5 w-5 border-2 bg-zinc-900 border-zinc-500 md:group-hover:border-zinc-300 rounded-full "></div>
           <div className="flex flex-col gap-2  text-base justify-between">
             <p className="  text-zinc-300 font-medium ">
-              Feature : select nfts as cover picture
-            </p>
-          </div>
-        </div>
-        <div className="group py-8 md:hover:bg-zinc-800/50 relative border-l-2 border-zinc-600 md:hover:border-zinc-400 pl-8 md:hover:pr-4  duration-300 easae-out">
-          <div className="absolute -left-[11px] h-5 w-5 border-2 bg-zinc-900 border-zinc-500 md:group-hover:border-zinc-300 rounded-full "></div>
-          <div className="flex flex-col gap-2  text-base justify-between">
-            <p className="  text-zinc-300 font-medium ">
               Introduce different types of designs for page
             </p>
           </div>
